f418d808aeb99937b84944c7d9a37f7827649330
GeorIsay <GeorIsay.personal.o@yandex.ru>
2023-07-06 15:20:04 +0300

    IGI: some arch update

--- src/AbstractSwarm.py
+++ src/AbstractSwarm.py
@@ -1,17 +1,19 @@
 import numpy as np
 from Particle import Particle
 import abc
+
+
 class AbstractSwarm(abc.ABC):
     def __init__(self, size: int, min_val: np.ndarray, max_val: np.ndarray, gwc: float, lwc: float):
         self.__size = size
-        assert len (min_val) == len (max_val)
+        assert len(min_val) == len(max_val)
         self.__min_val = min_val
         self.__max_val = max_val
         self.__gwc = gwc
         self.__lwc = lwc
         self.__best_particle = None
         self.__swarp = self.__creat_swarp()
-    
+
     def __creat_swarp(self):
         coef = (self.__max_val - self.__min_val) / (self.size + 1)
         cur = self.__max_val
@@ -20,20 +22,21 @@ class AbstractSwarm(abc.ABC):
             self.__swarp.append(Particle(cur, self))
             if self.__particle_comparer(self.__swarp[-1], self.__best_particle):
                 self.__best_particle = self.__swarp[-1]
-    
+
+    def best_particle(self):
+        return self.__best_particle
+
     @abc.abstractmethod
     def __particle_comparer(self, particle_1: Particle, particle_2: Particle):
         pass
-    
+
     @abc.abstractmethod
     def _func(self, particle: Particle):
         # оценка чистицы
         pass
-    
+
     @abc.abstractmethod
     def iterations(self, num, coef):
-        #делает сам алгоритм num шагов или пока среднее откланение не будет ниже coef
-        #возрашает лучшие коэф
-        
+        # делает сам алгоритм num шагов или пока среднее откланение не будет ниже coef
+
         pass
-    
--- src/Swarm.py
+++ src/Swarm.py
@@ -1,31 +1,40 @@
 from AbstractSwarm import AbstractSwarm
 from Particle import Particle
 import numpy as np
+
+
 class Swarm(AbstractSwarm):
     def __init__(self, size: int, min_val: np.ndarray, max_val: np.ndarray, gwc: float, lwc: float, x: np.ndarray, y: np.ndarray):
-        assert len (x) == len (y)
+        assert len(x) == len(y)
         # super().__init__(size, min_val, max_val, gwc, lwc)
         AbstractSwarm.__init__(self, size, min_val, max_val, gwc, lwc)
+        self.__mean_deviation_change = []
         self.__x = x.copy()
         self.__y = y.copy()
 
     def __particle_comparer(self, particle_1: Particle, particle_2: Particle):
-        #если первый лучше по оценке - True
+        # если первый лучше по оценке - True
         if particle_1 == None:
             return False
         elif particle_2 == None or particle_1.best_deviation() < particle_2.best_deviation():
             return True
         return False
-    
+
     def _func(self, particle: Particle):
         # оценка чистицы
         arr = particle.best_local_val()
         size = len(self.__x)
         return sum(abs(self.__y[i] - arr[0] * (self.__x[i] ** 3) - arr[1] * (self.__x[i] ** 2) - arr[2] * self.__x[i] - arr[3]) for i in range(size)) / size
-    
+
     def iterations(self, num, coef):
+        # сам алгоритм
+        # num - количество шагов
+        # coef - погрешность, при достижении которой можно не продолжать делать шаги до num
+        # с кождым откланение добовлять среднее откланение в self.__mean_deviation_change для будущего графика
         pass
-        # return self.__best_particle
-    
+
+    def mean_deviation_change(self):
+        return self.__mean_deviation_change
+
     def x_y(self):
         return self.__x, self.__y
